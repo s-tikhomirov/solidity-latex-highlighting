d9b0afacbe5cc128b90deb5cd1b8255ae7e8c953
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-09-08 15:17:07 +0200

    add example02

--- examples/example02.sol
+++ examples/example02.sol
@@ -1,3 +1,16 @@
 \begin{lstlisting}[language=Solidity]
+	pragma solidity 0.4.16;
 	
+	contract TestContract {
+	
+		string private myString = "foo";
+	
+		function getString() constant returns (string) {
+			return myString;
+		}
+	
+		function setString (string _string) {
+			myString = _string;
+		}
+	}
 \end{lstlisting}
