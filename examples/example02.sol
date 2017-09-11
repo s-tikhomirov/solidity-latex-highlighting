fd551d3df0a4a59c7c3ae4f50b75562c1154b19f
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-09-11 18:16:13 +0200

    update example to show env vars highlighting

--- examples/example02.sol
+++ examples/example02.sol
@@ -1,16 +1,18 @@
 \begin{lstlisting}[language=Solidity]
 	pragma solidity 0.4.16;
-	
+
 	contract TestContract {
 	
 		string private myString = "foo";
-	
-		function getString() constant returns (string) {
-			return myString;
+		uint private lastUpdated = now;
+		
+		function getString() constant returns (string, uint) {
+			return (myString, lastUpdated);
 		}
-	
+		
 		function setString (string _string) {
 			myString = _string;
+			lastUpdated = block.timestamp;
 		}
 	}
 \end{lstlisting}
